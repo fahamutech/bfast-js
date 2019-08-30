b1ba511a6f30ef4e9920b995fd2b7c2ec2757f89
Joshua <mama27j@gmail.com>
2019-08-30 17:39:27 +0300

    initial release

--- specs/domain.specs.js
+++ specs/domain.specs.js
@@ -1,29 +1,29 @@
-// // import {describe} from "mocha";
-//
-// var assert = require('assert');
+
+var assert = require('assert');
 // var BFast = require('../src/index');
-//
-// describe('Domain', function(){
-//
-//    //  var main;
-//     beforeEach(function(){
-//         BFast.init({
-//             serverUrl: 'https://ssm.fahamutech.com:8001',
-//             apiKey: 'ut778567ghj'
-//         });
-//     });
-//
-//     describe('get data(s)', function(){
-//         it('should get many data of a specific doamain', function(done){
-//             BFast.domain('invoices').many().then(values=>{
-//                 console.log(values);
-//                 done();
-//                 assert(1,1);
-//             }).catch(reason=>{
-//                 console.log(reason);
-//                 done(reason);
-//                 assert(1,1);
-//              });
-//         })
-//     })
-// });
+
+describe('Domain', function(){
+
+   //  var main;
+    // beforeEach(function(){
+    //     BFast.init({
+    //         serverUrl: 'https://ssm.fahamutech.com:8001',
+    //         apiKey: 'ut778567ghj'
+    //     });
+    // });
+
+    describe('get data(s)', function(){
+        it('should get many data of a specific doamain', function(done){
+            // BFast.domain('invoices').many().then(values=>{
+            //     console.log(values);
+            //     done();
+            //     assert(1,1);
+            // }).catch(reason=>{
+            //     console.log(reason);
+            //     done(reason);
+            //     assert(1,1);
+            //  });
+            assert(1,1);
+        })
+    })
+});
