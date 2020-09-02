1c0e237e268ba7270f106f357500b99a4f090da7
joshua <mama27j@gmail.com>
2020-09-02 13:58:23 +0300

    remove dtl in cache

--- src/factories/cache.ts
+++ src/factories/cache.ts
@@ -20,15 +20,15 @@ export class DefaultCacheFactory implements CacheAdapter {
         if (device.isNode) {
             return [];
         }
-        return DefaultCacheFactory._getCacheDatabase(database, collection)?.keys();
+        return DefaultCacheFactory._getCacheDatabase(database, collection).keys();
     }
 
     async clearAll(database: string, collection: string): Promise<boolean> {
         if (device.isNode) {
             return true;
         }
-        await DefaultCacheFactory._getCacheDatabase(database, collection)?.clear();
-        await DefaultCacheFactory._getCacheDatabase(database, '_ttl')?.clear();
+        await DefaultCacheFactory._getCacheDatabase(database, collection).clear();
+        //  await DefaultCacheFactory._getCacheDatabase(database, '_ttl')?.clear();
         return true;
     }
 
@@ -36,8 +36,8 @@ export class DefaultCacheFactory implements CacheAdapter {
         if (device.isNode) {
             return null as any;
         }
-        await this.remove(identifier, database, collection);
-        return DefaultCacheFactory._getCacheDatabase(database, collection)?.getItem(identifier) as any;
+        //  await this.remove(identifier, database, collection);
+        return DefaultCacheFactory._getCacheDatabase(database, collection).getItem(identifier) as any;
     }
 
     async set<T>(identifier: string, data: T, database: string, collection: string, options?: { dtl: number }): Promise<T> {
@@ -45,27 +45,29 @@ export class DefaultCacheFactory implements CacheAdapter {
             return null as any;
         }
         const response = await DefaultCacheFactory._getCacheDatabase(database, collection).setItem(identifier, data);
-        await DefaultCacheFactory._getCacheDatabase(database, '_ttl')?.setItem(identifier, DefaultCacheFactory._getDayToLeave(options ? options.dtl : 7));
+        // await DefaultCacheFactory._getCacheDatabase(database, '_ttl')?.setItem(identifier, DefaultCacheFactory._getDayToLeave(options ? options.dtl : 7));
         return response as any;
     }
 
-    private static _getDayToLeave(days: number) {
-        const date = new Date();
-        return date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    }
+    // private static _getDayToLeave(days: number) {
+    //     const date = new Date();
+    //     return date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    // }
 
     async remove(identifier: string, database: string, collection: string, force = false): Promise<boolean> {
         if (device.isNode) {
             return true;
         }
-        const dayToLeave = await DefaultCacheFactory._getCacheDatabase(database, '_ttl').getItem(identifier as any);
-        if (force || (dayToLeave && dayToLeave < new Date().getTime())) {
-            await DefaultCacheFactory._getCacheDatabase(database, '_ttl')?.removeItem(identifier);
-            await DefaultCacheFactory._getCacheDatabase(database, collection)?.removeItem(identifier);
-            return true;
-        } else {
-            return false;
-        }
+        await DefaultCacheFactory._getCacheDatabase(database, collection).removeItem(identifier);
+        return true;
+        // const dayToLeave = await DefaultCacheFactory._getCacheDatabase(database, '_ttl').getItem(identifier as any);
+        // if (force || (dayToLeave && dayToLeave < new Date().getTime())) {
+        //     await DefaultCacheFactory._getCacheDatabase(database, '_ttl')?.removeItem(identifier);
+        //     await DefaultCacheFactory._getCacheDatabase(database, collection)?.removeItem(identifier);
+        //     return true;
+        // } else {
+        //     return false;
+        // }
     }
 
     cacheEnabled(appName: string, options?: RequestOptions): boolean {
